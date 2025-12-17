63e893d6f078604ddb4889e4009b40c17a4d2e28
Nasccped <pedro.main.tec@gmail.com>
2025-12-17 22:55:44 +0000

    chore: update PS1 items

--- conf/.bashrc.d/ps1.sh
+++ conf/.bashrc.d/ps1.sh
@@ -1,36 +1,44 @@
 RESET_ESCAPE="\e[0m"
 RED_ESCAPE="\e[91m"
 GREEN_ESCAPE="\e[92m"
+YELLOW_ESCAPE="\e[93m"
 BLUE_ESCAPE="\e[94m"
 MAGENTA_ESCAPE="\e[95m"
 CYAN_ESCAPE="\e[96m"
 WHITE_ESCAPE="\e[97m"
 
 get_user() {
-  local luser=$(whoami)
-  echo -e "$RED_ESCAPE$luser$RESET_ESCAPE"
-}
-
-get_hostname() {
-  local lhostname=$(hostname)
-  echo -e "$CYAN_ESCAPE$lhostname$RESET_ESCAPE"
+  local usr="$(whoami 2>/dev/null)"
+  if [ "$usr" == "" ]; then
+    usr="undefined_user"
+  fi
+  echo "$usr"
 }
 
 get_curdir() {
-  local lcurdir=$(pwd)
-  if [ "$lcurdir" == $HOME ]; then
-    lcurdir="~"
+  local curdir="$(pwd 2>/dev/null)"
+  if [ "$curdir" == "" ]; then
+    lcurdir="undefined_dir"
+  elif [ "$curdir" == "$HOME" ]; then
+    curdir="~"
   else
-    lcurdir=$(echo $lcurdir | rev | cut -d'/' -f 1 | rev)
+    curdir="$(echo "$curdir" | rev | cut -d'/' -f 1 | rev)"
   fi
-  echo -e "$MAGENTA_ESCAPE$lcurdir$RESET_ESCAPE"
+  echo "$curdir"
 }
 
 get_ps1() {
-  local lu=$(get_user)
-  local lh=$(get_hostname)
-  local ld=$(get_curdir)
-  echo -e "$lc$lu$WHITE_ESCAPE@$lh $ld $GREEN_ESCAPE\$$RESET_ESCAPE "
+  local usr dir branch time cols spaces
+  usr="$(get_user)"
+  dir="$(get_curdir)"
+  time="$(date +%H:%M)"
+  cols=$(tput cols)
+  spaces=$(( cols - ${#usr} - ${#dir} - ${#branch} - ${#time} - 1 ))
+  printf '%b%s%b@%s%*s%b%s\n%b$ %b' \
+    "$CYAN_ESCAPE" "$usr" "$WHITE_ESCAPE" "$dir" \
+    "$spaces" "" \
+    "$YELLOW_ESCAPE" "$time" \
+    "$GREEN_ESCAPE" "$RESET_ESCAPE"
 }
 
-export PS1='$(get_ps1)'
+export PS1="$(get_ps1)"
